a4feaf9e5135e40f80c983ecd3b6ff2e6ff425f1
Askold Astakhov <astahov@element-soft.com>
2022-03-26 16:22:38 +0300

    CRM-1 env edition

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,9 @@
 export const _SYSTEM_NAME = 'WEBBER_CRM';
 
-export const _API_URL = 'http://localhost:8000';
+export const _IS_DEV = process.env.NODE_ENV === 'development';
+
+export const _IS_PROD = process.env.NODE_ENV === 'production';
+
+export const _API_URL = _IS_DEV ? 'http://localhost:8000' : 'https://webber-crm.herokuapp.com';
 
 export const _DEFAULT_NAME = 'Anonymous';
