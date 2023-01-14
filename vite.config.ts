6e6ff234f6a628fef5822afced3320257db2378e
Askold Astakhov <astahov@element-soft.com>
2023-01-14 15:06:15 +0300

    Vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 3000
+    },
+    build: {
+        outDir: './build',
+        minify: true,
+        reportCompressedSize: true,
+    }
 })
